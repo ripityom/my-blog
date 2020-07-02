e9a9ce1dcd6ee4cfa623497625b1f906185b538b
ripityom <67728642+ripityom@users.noreply.github.com>
2020-07-02 08:16:40 +0100

    First commit

---
+++ src/build/precache-manifest.ab7c404d69fc5a59ba88babe1173c896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75baea127ff997984454d2a9de5cb34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcd78e8d7b0c550dea71",
+    "url": "/static/css/main.659d1945.chunk.css"
+  },
+  {
+    "revision": "7ba9fc26b8f5b228091d",
+    "url": "/static/js/2.0534f611.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0534f611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd78e8d7b0c550dea71",
+    "url": "/static/js/main.dafb89b5.chunk.js"
+  },
+  {
+    "revision": "210a08156c142e5a7a2d",
+    "url": "/static/js/runtime-main.5f93d7e2.js"
+  }
+]);
